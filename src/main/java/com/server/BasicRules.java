e173cb55f8ddf1604d3518eb768b2690c671693b
malinowska423 <malinowska423@gmail.com>
2018-12-12 00:13:35 +0100

    Room and Rules basic implementation

--- src/main/java/com/server/BasicRules.java
+++ src/main/java/com/server/BasicRules.java
@@ -1,4 +1,15 @@
 package com.server;
 
 public class BasicRules implements Rules {
+    @Override
+    public boolean checkIfWon(Player player) {
+        //TODO: implement method
+        return false;
+    }
+
+    @Override
+    public String showPossibleMoves() {
+        //TODO: implement method
+        return null;
+    }
 }
