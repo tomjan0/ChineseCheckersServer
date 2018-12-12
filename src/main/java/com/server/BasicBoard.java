0dd2f350c79a5a856edf6f7544ae79ddd0ec0416
malinowska423 <malinowska423@gmail.com>
2018-12-12 04:45:25 +0100

    Board improvement

--- src/main/java/com/server/BasicBoard.java
+++ src/main/java/com/server/BasicBoard.java
@@ -2,7 +2,7 @@ package com.server;
 
 public class BasicBoard extends Board {
     public BasicBoard() {
-        super(new BoardField[19][15]);
+        super(19,15);
         this.createField(1,7);
         this.createField(2,6,2);
         this.createField(3,6,3);
@@ -20,12 +20,6 @@ public class BasicBoard extends Board {
         this.createField(15,6,3);
         this.createField(16,6,2);
         this.createField(17,7);
-    }
-
-
-    @Override
-    public BoardField getField(int y, int x) {
-        return this.fieldArray[y][x];
     }
 
     @Override
--- src/main/java/com/server/Board.java
+++ src/main/java/com/server/Board.java
@@ -3,11 +3,13 @@ package com.server;
 public abstract class Board {
     BoardField[][] fieldArray;
 
-    public Board(BoardField[][] fieldArray) {
-        this.fieldArray = fieldArray;
+    public Board(int rows, int cols) {
+        this.fieldArray = new BoardField[rows][cols];
     }
 
-    public abstract BoardField getField(int y, int x);
+    public BoardField getField(int y, int x) {
+        return this.fieldArray[y][x];
+    }
     public abstract void addPlayer(Player player);
 
     public void createField(int y, int x){
