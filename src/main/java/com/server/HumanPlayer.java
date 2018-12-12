46fc182cc97e1b03ac694efbf4518b4b811c61d1
Tomek <slimak98@gmail.com>
2018-12-12 03:50:16 +0100

    Added Player and Room Constructors

--- src/main/java/com/server/HumanPlayer.java
+++ src/main/java/com/server/HumanPlayer.java
@@ -5,9 +5,14 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class HumanPlayer extends Player {
-    private Socket playerSocket;
+    private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
+
+    public HumanPlayer(int id, Socket socket){
+        super(id);
+        this.socket = socket;
+    }
 
     public void sendMessage(String message) {
         out.print(message);
--- src/main/java/com/server/Player.java
+++ src/main/java/com/server/Player.java
@@ -3,9 +3,16 @@ package com.server;
 
 public abstract class Player {
     private int playerId;
+    private boolean isPlaying;
     private boolean isMoving;
     private int pawnsInPlace;
 
+    public Player(int id){
+        playerId = id;
+        isPlaying = false;
+        isMoving = false;
+        pawnsInPlace = 0;
+    }
 
     public int getPlayerId() {
         return playerId;
