7209711b8c521f9b6143da7bc0778656ab94dd54
Tomasz <slimak98@gmail.com>
2019-01-03 04:35:32 +0100

    Exchanging ports with client

--- src/main/java/com/server/Server.java
+++ src/main/java/com/server/Server.java
@@ -1,6 +1,7 @@
 package com.server;
 
 import com.sun.source.tree.WhileLoopTree;
+import test.TestGUI;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,70 +14,50 @@ import java.util.ArrayList;
 
 public class Server {
 
-    private static final int PORT = 2137;
+    private static int port = 1025;
     private static ArrayList<Room> roomList = new ArrayList<>();
     private static ArrayList<Player> playerList = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        BufferedReader in;
-        PrintWriter out;
-        System.out.println("Server is running");
-        try {
-            ServerSocket listener = new ServerSocket(PORT);
-            while(true) {
-                try {
-                    Socket socket = listener.accept();
-                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    out = new PrintWriter(socket.getOutputStream(), true);
-                    System.out.println("Client connected");
-
-                    String input = "";
+        System.out.println("Starting server");
 
 
-                    while (!(input.equals("//end"))) {
-                        input = in.readLine();
-                        System.out.println(input);
+        System.out.println("Entering listening loop");
+        while (true) {
+            try {
+
+                ServerSocket serverSocket = new ServerSocket(1024);
+
+                System.out.println("Waiting for connection...");
+
+                Socket clientSocket = serverSocket.accept();
+
+                System.out.println(clientSocket.getInetAddress().toString() + " Connected");
+
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String message = "";
+
+                while ((message = in.readLine()) != null) {
+                    System.out.println(message);
+                    if(message.equals("port-request")) {
+                        out.println("p" + port);
+                        port++;
                     }
-                    out.println("Conection established");
-
-                    for (Room room : roomList) {
-                        out.println(room.toString());
+                    if(message.equals("pend")){
+                        out.println("disconnected");
+                        break;
                     }
-                    out.println("CREATE ROOM?");
-                    out.println("//end");
-
-                    input = "";
-                    Room room = null;
-
-                    while(!(input.equals("//end"))) {
-                        input = in.readLine();
-                        if (input.equals("Y")) {
-                            input = in.readLine();
-                            int number = Integer.parseInt(input);
-                            Player player = new HumanPlayer(playerList.size(), socket);
-                            room = new Room(roomList.size(), player, number, 1);
-                            roomList.add(room);
-                        }
-                    }
-                    out.println("Your room:\n    " + room.toString());
-                    out.println("//end");
-
-                    input = "";
-                    while (!(input.equals("//end"))) {
-                        input = in.readLine();
-                        System.out.println(input);
-                    }
-
-
-                    socket.close();
                 }
-                catch (IOException ex){
-                    System.out.println("Connection to client failed");
-                }
+
+                clientSocket.close();
+
+            } catch (IOException ex) {
+//                System.out.println("Exception");
             }
-        } catch (IOException e) {
-            System.out.println("Creating socket failed");
+
         }
     }
 }
--- src/main/java/com/server/ServerThread.java
+++ src/main/java/com/server/ServerThread.java
@@ -1,9 +1,59 @@
 package com.server;
 
+import test.TestGUI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerThread implements Runnable {
+    Socket clientSocket;
+    PrintWriter out;
+    BufferedReader in;
+
+    public ServerThread(int port) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            System.out.println("Waiting for connection...");
+
+            clientSocket = serverSocket.accept();
+
+            System.out.println(clientSocket.getInetAddress().toString() + " Connected");
+
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        } catch (IOException ex) {
+//                System.out.println("Exception");
+        }
+    }
 
     @Override
     public void run() {
+        try {
+            String message = "";
+
+            while ((message = in.readLine()) != null) {
+                System.out.println(message);
+                if(message.equals("port-request")) {
+
+                }
+                if(message.equals("pend")){
+                    out.println("disconnected");
+                    break;
+                }
+            }
+
+            clientSocket.close();
+
+        } catch (IOException ex) {
+//                System.out.println("Exception");
+        }
 
     }
 }
