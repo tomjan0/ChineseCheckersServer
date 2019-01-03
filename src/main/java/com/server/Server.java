9d8c056899ce0de28af9397704a5d205400a1d02
malinowska423 <amalinowska423@gmail.com>
2019-01-03 19:53:49 +0100

    Connection to client modified

--- src/main/java/com/server/Server.java
+++ src/main/java/com/server/Server.java
@@ -1,70 +1,30 @@
 package com.server;
 
-import com.sun.source.tree.WhileLoopTree;
-//import test.TestGUI;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
 
-    private static int port = 1025;
+    private static final int PORT = 7554;
     private static ArrayList<Room> roomList = new ArrayList<>();
     private static ArrayList<Player> playerList = new ArrayList<>();
 
     public static void main(String[] args) {
 
         System.out.println("Starting server");
-
-
-        System.out.println("Entering listening loop");
-        while (true) {
-            try {
-
-                ServerSocket serverSocket = new ServerSocket(1024);
-
-                System.out.println("Waiting for connection...");
-
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Waiting for the first connection...");
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
-
-                System.out.println(clientSocket.getInetAddress().toString() + " Connected");
-
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                String message = "";
-
-                while ((message = in.readLine()) != null) {
-                    System.out.println("\tClient message: " + message);
-                    if(message.equals("port-request")) {
-                        out.println(port);
-                        port++;
-                    }
-                    if (message.equals("Player")) {
-                        out.println("445");
-                        System.out.println(message + "#445");
-                    }
-                    if (message.equals("new-room")){
-                        out.println("1;4;3;Basic;2;3;4;6;1;2;3");
-                    }
-                    if(message.equals("pend")){
-                        System.out.println("Disconnected");
-                        break;
-                    }
-                }
-
-                clientSocket.close();
-
-            } catch (IOException ex) {
-//                System.out.println("Exception");
+                ServerThread client = new ServerThread(clientSocket);
+                Thread game = new Thread(client);
+                game.start();
             }
-
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/main/java/com/server/ServerThread.java
+++ src/main/java/com/server/ServerThread.java
@@ -6,54 +6,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerThread implements Runnable {
-    Socket clientSocket;
-    PrintWriter out;
-    BufferedReader in;
 
-    public ServerThread(int port) {
+    private PrintWriter out;
+    private BufferedReader in;
+    private Socket clientSocket;
 
+    public ServerThread(Socket socket) {
         try {
-            ServerSocket serverSocket = new ServerSocket(port);
-
-            System.out.println("Waiting for connection...");
-
-            clientSocket = serverSocket.accept();
-
+            clientSocket = socket;
             System.out.println(clientSocket.getInetAddress().toString() + " Connected");
-
             out = new PrintWriter(clientSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        } catch (IOException ex) {
-//                System.out.println("Exception");
+        } catch (IOException e) {
+            closeSocket();
         }
     }
 
     @Override
     public void run() {
         try {
-            String message = "";
-
-            while ((message = in.readLine()) != null) {
-                System.out.println(message);
-                if(message.equals("port-request")) {
-
-                }
-                if(message.equals("pend")){
-                    out.println("disconnected");
-                    break;
-                }
-            }
-
-            clientSocket.close();
-
+            String message = in.readLine();
+            do {
+                System.out.println("\tClient request: " + message);
+                chooseResponse(message);
+                message = in.readLine();
+            } while (message != null);
         } catch (IOException ex) {
 //                System.out.println("Exception");
         }
+    }
 
+    private void chooseResponse(String message) {
+        if (message.equals("pend")) {
+            closeSocket();
+        } else {
+            //TODO: call method that runs appropriate code depending on message
+            if (message.equals("port-request")) {
+                out.println("6363");
+            }
+            if (message.equals("Player")) {
+                out.println("492");
+            }
+            if (message.equals("new-room")){
+                out.println("1;4;3;Basic;2;3;4;6;1;2;3");
+            }
+        }
+    }
+
+    public void closeSocket() {
+        try {
+            clientSocket.close();
+            System.out.println(clientSocket.getInetAddress().toString() + " Disconnected");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
